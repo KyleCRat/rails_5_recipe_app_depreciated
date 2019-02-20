f72d45fe5424a2a1085e3d46bcee8e7e10813679
Kyle Ratliff <KyleCRat@me.com>
2019-02-20 11:30:09 -0500

    initailzie ajax recipe_step editing on recipe page

--- app/helpers/recipe_steps_helper.rb
+++ app/helpers/recipe_steps_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module RecipeStepsHelper
+  def render_recipe_step_ajax_edit_field(step)
+    model_name = step.model_name.human
+    param_key  = step.model_name.param_key
+    id         = step.id
+    html_key   = "recipe_step_edit_#{param_key}_#{id}"
+
+    html = content_tag(:div, id: html_key) do
+      link_to model_name, remote_form_step_path(step), method: :post, remote: true
+    end
+
+    html.html_safe
+  end
 end
