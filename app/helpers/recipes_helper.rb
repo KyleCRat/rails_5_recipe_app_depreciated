b59c0699ab7707c57476e39de57d6442649f4fbd
Kyle Ratliff <KyleCRat@me.com>
2019-05-04 12:42:09 -0400

    make more readable

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -4,20 +4,20 @@ module RecipesHelper
   def render_combined_ingredients(recipe, form)
     html = []
 
-    ing_groups = recipe.all_ingredients.group_by { |si| si.ingredient.title }
+    ingredient_groups = recipe.all_ingredients.group_by { |si| si.ingredient.title }
 
-    ing_groups.sort.each do |_title, group|
-      temp_s_ing = group.first.dup
-      temp_s_ing.measurements = []
-      temp_s_ing.technique = nil if group.many?
+    ingredient_groups.sort.each do |_title, group|
+      step_ingredient = group.first.dup
+      step_ingredient.measurements = []
+      step_ingredient.technique = nil if group.many?
 
       group.each do |ingredient|
         ingredient.measurements.each do |measurement|
-          temp_s_ing.measurements << measurement
+          step_ingredient.measurements << measurement
         end
       end
 
-      html << render(temp_s_ing, f: form)
+      html << render(step_ingredient, f: form)
     end
 
     html.sort.join.html_safe
