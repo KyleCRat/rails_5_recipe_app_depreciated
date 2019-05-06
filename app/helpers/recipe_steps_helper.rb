d162d3cefdbdc1556905ec3a15af0a718b7af499
Kyle Ratliff <KyleCRat@me.com>
2019-05-06 11:58:53 -0400

    fix up edit stepable fields

--- app/helpers/recipe_steps_helper.rb
+++ app/helpers/recipe_steps_helper.rb
@@ -1,21 +1,21 @@
 module RecipeStepsHelper
-  def render_recipe_step_ajax_edit_field(step)
-    model_name = step.model_name.human
-    param_key  = step.model_name.param_key
-    route_key  = step.model_name.route_key
-    id         = step.id
+  # take a stepable relation to a recipe and render the correct html to edit
+  #   the relationship on the recipes edit page
+  def render_recipe_step_ajax_edit_field(stepable)
+    model_name = stepable.model_name.human
+    param_key  = stepable.model_name.param_key
+    route_key  = stepable.model_name.route_key
+    id         = stepable.id
     html_key   = "recipe_step_edit_#{param_key}_#{id}"
     btn_key    = html_key + '_btn'
 
-    html = content_tag(:div) do
-      if step.is_a? Step
-        content_tag(:div, render(step).html_safe, id: html_key) +
-          link_to("Edit #{model_name}", edit_step_path(step), remote: true, id: btn_key)
-      else
-        link_to("View #{model_name}",
-                "/#{route_key}/#{id}/edit",
-                onclick: "return confirm('Are you sure you want to quit editing this recipe?')")
-      end
+    if stepable.is_a? Step
+      html = content_tag(:div, render(stepable).html_safe, id: html_key) +
+             link_to("Edit #{model_name}", edit_step_path(stepable), remote: true, id: btn_key)
+    else
+      html = link_to("View #{model_name}",
+                     "/#{route_key}/#{id}/edit",
+                     onclick: "return confirm('Are you sure you want to quit editing this recipe?')")
     end
 
     html.html_safe
